a7839a2841fc201fbdcb5456c9f90c6c7aa3e1ed
royndegroot <roy.n.degroot@gmail.com>
2016-01-04 22:54:40 +0100

    SFML Logo test
    Loading a texture and displaying a sprite

--- Tech/Cavemen/Main.cpp
+++ Tech/Cavemen/Main.cpp
@@ -1,19 +1,30 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-	sf::RenderWindow window( sf::VideoMode( 200, 200 ), "SFML works!" );
+	sf::RenderWindow window( sf::VideoMode( 800, 480 ), "Cavemen" );
 	sf::CircleShape shape( 100.f );
 	shape.setFillColor( sf::Color::Green );
+
+	sf::Texture texture;
+	if ( !texture.loadFromFile( "Images/logo-cavemen.png" ) ) {
+		// Handle failure
+	}
+
+	sf::Sprite sprite;
+	sf::FloatRect bounds = sprite.getGlobalBounds();
+	sprite.setPosition( (800 - 392) * 0.5f, (480 - 128) * 0.5f );
+	sprite.setTexture( texture );
 
 	while ( window.isOpen() ) {
 		sf::Event event;
 		while ( window.pollEvent( event ) ) {
-			if ( event.type == sf::Event::Closed )
+			if ( event.type == sf::Event::Closed || ( event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape ) ) {
 				window.close();
+			}
 		}
 
 		window.clear();
-		window.draw( shape );
+		window.draw( sprite );
 		window.display();
 	}
 
